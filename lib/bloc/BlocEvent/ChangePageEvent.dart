28bbc3d48b3867406bae0d257ac5e8ea8f27d13b
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2022-05-24 23:42:33 +0700

    .

--- lib/bloc/BlocEvent/ChangePageEvent.dart
+++ lib/bloc/BlocEvent/ChangePageEvent.dart
@@ -13,10 +13,15 @@ abstract class ChangePage_Event {}
 
 class ChangePage extends ChangePage_Event {}
 
+class ChangePage_nodrower extends ChangePage_Event {}
+
 class ChangePage_Bloc extends Bloc<ChangePage_Event, Widget> {
   ChangePage_Bloc() : super(const Page0()) {
     on<ChangePage>((event, emit) {
       return _ChangePage_Function(state, emit);
+    });
+    on<ChangePage_nodrower>((event, emit) {
+      return _ChangePage_Function_no_drower(state, emit);
     });
   }
   Future<void> _ChangePage_Function(Widget toAdd, Emitter<Widget> emit) async {
@@ -35,6 +40,27 @@ class ChangePage_Bloc extends Bloc<ChangePage_Event, Widget> {
     //   //     .UpdateNotification("", "Error", enumNotificationlist.Error);
 
     Navigator.pop(MenuContext);
+    // print(output);
+    emit(CuPage);
+  }
+
+  Future<void> _ChangePage_Function_no_drower(
+      Widget toAdd, Emitter<Widget> emit) async {
+    // print('${CuPage}');
+
+    Widget output = Page0();
+    // MainBodyContext
+    // MenuContext
+
+    if (UserLV >= CuPageLV) {
+    } else {
+      CuPage = Page0();
+    }
+
+    //   // BlocProvider.of<BlocNotification>(contextGB)
+    //   //     .UpdateNotification("", "Error", enumNotificationlist.Error);
+
+    // Navigator.pop(MenuContext);
     // print(output);
     emit(CuPage);
   }
